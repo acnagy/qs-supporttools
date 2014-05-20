1ace76bd874a039bab5048a131e99ed694b6a97f
Rick <rick.c.nagy@gmail.com>
2014-05-20 16:17:28 -0400

    modularize qs-controller and add resources

--- qs-controller.js
+++ qs-controller.js
@@ -10,12 +10,26 @@
 
 /* inject QSIterator.js into the page.
  * Makes QSIterator a globally accessible variable on QS pages
+ * note that it means that all script injection is done once all resources have been loaded
+ * 		this is to avoid slowing down page rendering
  */
-if (document.URL.match("quickschools")) {
+$(window).load(function() {
+	if (document.URL.match("quickschools.com")
+			|| document.URL.match("smartschoolcentral.com")) {
+		injectScript("QSIterator.js");
+		injectScript("QSTableIterator.js")
+		injectScript("/JS.Class/src/loader-browser.js", false);
+		
+		jsClassSrc = chrome.extension.getURL("/JS.Class/src/loader-browser.js");
+		$("head").append($("<script>JSCLASS_PATH = '" + jsClassSrc + "'</script>"))
+	}
+})
+
+function injectScript(filename, isLocal) {
+	var isLocal = isLocal || true;
 	var s = document.createElement("script");
-	s.src = chrome.extension.getURL("QSIterator.js");
-	s.onLoad = function() {
-	    this.parentNode.removeChild(this);
-	};
+	s.src = (isLocal || typeof isLocal === "undefined")
+		? chrome.extension.getURL(filename)
+		: filename;
 	(document.head||document.documentElement).appendChild(s);
 }
